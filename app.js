4a906352f84739f3b865bf2d90763e8c8d655b54
mileycent <39402443+mileycent@users.noreply.github.com>
2023-07-19 14:28:42 -0700

    Displaying the temperature Elements in real time.

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 function displayCurrentTemperature(response) {
 
-  let temperatureElement.innerHTML = document.querySelector("#temperature");
-  temperatureElement.innerHTML = response.data.main.temp;
-  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  // to change the city written in html doc to API version 
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML =  response.data.name;
+  // to change the desription writen in html doc to API version
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  // to change the win, preciptitation and wind description written in html doc
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 
 }
 let apiKey = "6a48a550fc04f170639e60d52b8a6bc5";
