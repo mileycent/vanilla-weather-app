dc9de172098a65d7796c9fbf5c0399930a46ec6c
mileycent <39402443+mileycent@users.noreply.github.com>
2023-07-19 15:08:55 -0700

    Displayed current date and time

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+  let days = ["Sunday","Monday", "Tuesday","Wednesday", "Thursday","Friday","Saturday"];
+  let day = days[date.getDay()];
+  return `${day} "|" ${hours}:${minutes}`;
+}
+
+
 function displayCurrentTemperature(response) {
 
   let temperatureElement = document.querySelector("#temperature");
@@ -8,11 +25,14 @@ function displayCurrentTemperature(response) {
   // to change the desription writen in html doc to API version
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
-  // to change the win, preciptitation and wind description written in html doc
+  // to change the wind, preciptitation and wind description written in html doc
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  // to change the date
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
 }
 let apiKey = "6a48a550fc04f170639e60d52b8a6bc5";
