69db8bfafa1328201a321344c0bd8fb3113864ba
mileycent <39402443+mileycent@users.noreply.github.com>
2023-07-19 15:33:34 -0700

    Displayed current icon from weather API

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if (hours < 10) {
 }
   let days = ["Sunday","Monday", "Tuesday","Wednesday", "Thursday","Friday","Saturday"];
   let day = days[date.getDay()];
-  return `${day} "|" ${hours}:${minutes}`;
+  return `${day} | ${hours}:${minutes}`;
 }
 
 
@@ -33,9 +33,15 @@ function displayCurrentTemperature(response) {
   // to change the date
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  // to change the icon
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  // to change the alternative icon text
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 
 }
 let apiKey = "6a48a550fc04f170639e60d52b8a6bc5";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=${apiKey}&units=metric`;
+let city = "Mozambique";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayCurrentTemperature);
