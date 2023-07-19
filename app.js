53148371264fbc6fe3e856ee394020c2816cc699
mileycent <39402443+mileycent@users.noreply.github.com>
2023-07-19 10:31:49 -0700

    Displaying current temperature of city

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-let iconAPI = "1ayWzi8gBs3iXu2u2VYjmVSLhI9wCQxICwhBcmqDqyAvzhbS";
+function displayCurrentTemperature(response) {
+
+  let temperatureElement.innerHTML = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.main.temp;
+  console.log(response.data.main.temp);
+
+}
+let apiKey = "6a48a550fc04f170639e60d52b8a6bc5";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayCurrentTemperature);
